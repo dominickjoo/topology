ffb1d6a82c0b50b6450b200b619ac3aef4db3dda
Dominick Joo <dominickijoo@gmail.com>
2021-08-13 19:34:04 -0400

    add power_set to nt.py

--- nt.py
+++ nt.py
@@ -1,4 +1,5 @@
 import math
+import itertools
 from functools import reduce
 
 # Returns True if given number is prime, else False
@@ -13,3 +14,8 @@ def is_prime(num):
 # Returns the greatest common divisor of all the integers in the given list
 def gcd_mult(nums):
     return reduce(math.gcd, nums)
+
+# Returns the power set of a given iterable WITHOUT the empty set
+def power_set(iterable):
+    s = list(iterable)
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
